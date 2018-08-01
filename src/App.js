34f17d84e11d5aef053f9e2c0206879fdc0f0a97
ijzn <592193834@qq.com>
2018-08-01 17:30:28 +0800

    项目初始化，项目布局完成

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Admin from './admin'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Admin />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/admin.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Row, Col } from 'antd';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+import './style/common.less'
+export default class admin extends Component {
+  
+  render() {
+    return (
+      <Row className="container">
+        <Col span={3} className="nav-left">
+          <NavLeft />
+        </Col>
+        <Col span={21} className="main">
+          <Header />
+          <Row className="contant">
+            contant
+            {/* {this.props.children} */}
+          </Row>
+          <Footer />
+        </Col>
+      </Row>
+    )
+  }
+}
