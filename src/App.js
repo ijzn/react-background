cca7f7bdb92389480c343760fd1be032d52c33e6
ijzn <592193834@qq.com>
2018-08-03 14:27:46 +0800

    左侧导航完成

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Admin from './admin'
+import Admin from './admin.js'
 class App extends Component {
   render() {
     return (
--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,43 +1,46 @@
-import React, { Component } from 'react'
-import menuConfig from './../../config/menuConfig';
-import { Menu, Icon } from 'antd';
-function handleClick(e) {
-  console.log('click', e);
-}
-export default class NavLeft extends Component {
-  render() {
-    const SubMenu = Menu.SubMenu;
-    const MenuItemGroup = Menu.ItemGroup
-    const MenuItem = Menu.Item
-    return (
-      <div>
-        <Menu onClick={handleClick} style={{ width: 256 }} theme="dark" mode="vertical">
-          <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-            <MenuItemGroup title="Item 1">
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
-            </MenuItemGroup>
-            <MenuItemGroup title="Iteom 2">
-              <MenuItem key="3">Option 3</MenuItem>
-              <MenuItem key="4">Option 4</MenuItem>
-            </MenuItemGroup>
-          </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-            <MenuItem key="5">Option 5</MenuItem>
-            <MenuItem key="6">Option 6</MenuItem>
-            <SubMenu key="sub3" title="Submenu">
-              <MenuItem key="7">Option 7</MenuItem>
-              <MenuItem key="8">Option 8</MenuItem>
-            </SubMenu>
-          </SubMenu>
-          <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
-            <MenuItem key="9">Option 9</MenuItem>
-            <MenuItem key="10">Option 10</MenuItem>
-            <MenuItem key="11">Option 11</MenuItem>
-            <MenuItem key="12">Option 12</MenuItem>
-          </SubMenu>
-        </Menu>
-      </div>
-    )
-  }
+import React from 'react'
+import { Menu } from 'antd';
+// import { NavLink } from 'react-router-dom'
+import MenuConfig from './../../config/menuConfig'
+import './index.less'
+const SubMenu = Menu.SubMenu;
+export default class NavLeft extends React.Component {
+    componentWillMount(){
+        const menuTreeNode = this.renderMenu(MenuConfig);
+
+        this.setState({
+            menuTreeNode
+        })
+    }
+    // 菜单渲染
+    renderMenu =(data)=>{
+        return data.map((item)=>{
+            if(item.children){
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        { this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item title={item.title} key={item.key}>
+                {/* <NavLink to={item.key}>{item.title}</NavLink> */}
+                {item.title}
+            </Menu.Item>
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" alt=""/>
+                    <h1>Imooc MS</h1>
+                </div>
+                <Menu
+                    theme="dark"
+                >
+                    { this.state.menuTreeNode }
+                </Menu>
+            </div>
+        );
+    }
 }
